355e47705f6a0fd4ff4a254befa330b1ea2f3122
Avalon <jpmagtang@gmail.com>
2024-01-27 09:54:07 +0800

    #8 - chore(database): create a system database.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,40 @@
-import fileServer from "node:fs"
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-prisma.product.create({
-    data: {
-        id: "1",
-        name: "Multiuse Wrap Summer Cover Up Skirt by Karisma.Co",
-        description: "Yez",
-        price: "399",
-    }
-});
+// NOT WORKING
+
+// async function main() {
+//     const product = prisma.product.create({
+//         data: {
+//             name: `Multiuse Wrap Summer Cover Up Skirt by Karisma.Co`,
+//             description: `NEW PRODUCT ALERT❗
+    
+//             Karisma.Co's Multiuse Wrap Summer Cover Up Skirt
+//             📌 Our wrap skirt is made of Charmeuse Silk. 
+//             📌 Why Silk?
+//             When temperatures are high, the material lets excess heat escape. Likewise, when temperatures are cold, the material keeps the heat in. As a result, silk charmeuse is the epitome of comfort in all seasons.
+            
+//             📌 Unlike cotton and similar materials, silk charmeuse does not absorb moisture. This means that it will not absorb the natural oils and skin care products that can cause skin to become dry and dull. 
+            
+            
+//             📌 Should you have any questions or inquiries, feel free to connect with us and we will get back to you once we see your message.
+//             📌 Once your order has been approved (to ship), we cannot cancel it because of Shopee's Policies.
+            
+//             Happy shopping! Choose Karisma!✨💛`,
+//             price: 399.00,
+//         }
+//     });
+  
+//   console.log({ product })
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
